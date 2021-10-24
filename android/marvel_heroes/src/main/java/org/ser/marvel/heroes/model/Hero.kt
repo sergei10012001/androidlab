1298f89bc2c302aec43a9e7e4393d5dced9aa264
Sergey <sergei10012001@mail.ru>
2021-10-24 16:45:12 +0600

    ios1

--- android/marvel_heroes/src/main/java/org/ser/marvel/heroes/model/Hero.kt
+++
@@ -1,6 +0,0 @@
-package org.ser.marvel.heroes.model
-
-import androidx.annotation.ColorInt
-import androidx.annotation.DrawableRes
-
-class Hero(val id: Int, val name: String, @DrawableRes val avatar: Int, @ColorInt val color: Int)
